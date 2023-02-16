760a43b0b0aae997f981fda58b1f8a62010f58e4
Pedroscsilva <pedroscsilva2001@gmail.com>
2023-02-16 17:32:48 -0300

    feat: req07 - adding a Character class

--- src/Archetypes/Archetype.ts
+++ src/Archetypes/Archetype.ts
@@ -1,19 +1,23 @@
 import { EnergyType } from '../Energy';
 
 abstract class Archetype {
-  readonly name: string;
-  readonly special: number;
-  readonly cost: number;
+  _name: string;
+  _special: number;
+  _cost: number;
 
   constructor(name: string) {
-    this.name = name;
-    this.special = 0;
-    this.cost = 0;
+    this._name = name;
+    this._special = 0;
+    this._cost = 0;
   }
 
   static createdArchetypeInstances(): number {
     throw new Error('Not implemented');
   }
+
+  get name() { return this._name; }
+  get special() { return this._special; }
+  get cost() { return this._cost; }
 
   abstract get energyType(): EnergyType;
 }
--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,15 +1,18 @@
 abstract class Race {
-  readonly name: string;
-  readonly dexterity: number;
+  private _name: string;
+  private _dexterity: number;
 
   constructor(name: string, dexterity: number) {
-    this.name = name;
-    this.dexterity = dexterity;
+    this._name = name;
+    this._dexterity = dexterity;
   }
 
   static createdRacesInstances(): number {
     throw new Error('Not implemented');
   }
+
+  get name() { return this._name; }
+  get dexterity() { return this._dexterity; }
 
   abstract get maxLifePoints(): number;
 }
